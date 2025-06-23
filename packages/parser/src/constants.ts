d3d84b3e160c321d57b1ecb18d48ba685912483e
Dan Lynch <pyramation@gmail.com>
2025-06-23 14:47:10 -0700

    ready for Archival

--- packages/parser/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const SPECIAL_TYPES = ['TypeName'];
-// export const SPECIAL_TYPES = ['TypeName', 'RangeVar'];
-
-export const NODE_TYPE = 'Node';
--- packages/parser/types/constants.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const SPECIAL_TYPES: string[];
-export declare const NODE_TYPE = "Node";
